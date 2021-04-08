ab8ea4bc1bacdbad57e7ec78eeeac9b5aa21d8e5
Fathor Rahman <fathorrahman2357@gmail.com>
2021-04-08 13:39:30 +0700

    Summary

--- Laravel/resources/views/frontend/home.blade.php
+++ Laravel/resources/views/frontend/home.blade.php
@@ -56,4 +56,5 @@
 
     </div>
   </section><!-- End About Section -->
+
   @endsection
--- Laravel/resources/views/frontend/layouts/template.blade.php
+++ Laravel/resources/views/frontend/layouts/template.blade.php
@@ -743,18 +743,18 @@
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="{{ asset('backend/assets/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/venobox/venobox.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/counterup/counterup.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{ asset('backend/assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="{{ asset('backend/assets/js/main.js')}}"></script>
 
 </body>
 
